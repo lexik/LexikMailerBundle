72be3e67db166c102734ce7ee640f94db0ef1dfb
Onno Schmidt <onno.schmidt@gmail.com>
2012-12-12 14:11:50 +0100

    Extended mocked doctrine configuration to include getQuoteStrategy and returns the DefaultQuoteStrategy

--- Tests/Unit/BaseUnitTestCase.php
+++ Tests/Unit/BaseUnitTestCase.php
@@ -3,6 +3,7 @@
 namespace Lexik\Bundle\MailerBundle\Tests\Unit;
 
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\DefaultQuoteStrategy;
 use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\Common\Annotations\AnnotationReader;
@@ -83,6 +84,9 @@ abstract class BaseUnitTestCase extends \PHPUnit_Framework_TestCase
         $config->expects($this->any())
             ->method('getDefaultRepositoryClassName')
             ->will($this->returnValue('Doctrine\\ORM\\EntityRepository'));
+        $config->expects($this->any())
+            ->method('getQuoteStrategy')
+            ->will($this->returnValue(new DefaultQuoteStrategy()));
 
         $conn = array(
             'driver' => 'pdo_sqlite',
